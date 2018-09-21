d55c46f19b6dce4cc268acbaa52af697e9b393dd
Danil.Kurkin <danil.kurkin@reedonline.co.uk>
2018-09-21 15:06:21 +0100

    First query UnitTest and Code complete version

--- MunroLibrary/MunroQueries.cs
+++ MunroLibrary/MunroQueries.cs
@@ -1,31 +1,42 @@
 ﻿using MunroLibrary.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using MunroLibrary.Model;
 
 namespace MunroLibrary
 {
     public class MunroQueries
     {
-        public List<string[]> SortByCategory(HillCategory hillCategory)
+        public List<Munro> SortByCategory(List<Munro> munroData, HillCategory hillCategory)
         {
-            switch (hillCategory)
+            if (munroData?.Count > 0)
             {
-                case HillCategory.Munro:
-                    {
-                        break;
-                    }
-                case HillCategory.MunroTop:
-                    {
-                        break;
-                    }
-                default:
-                    {
-                        break;
-                    }
-            }
 
-            return new List<string[]>();
+                switch (hillCategory)
+                {
+                    case HillCategory.Munro:
+                    {
+                        return munroData.Where(element => element.Category.Equals("MUN")).ToList();
+                        break;
+                    }
+                    case HillCategory.MunroTop:
+                    {
+                        return munroData.Where(element => element.Category.Equals("TOP")).ToList();
+                        break;
+                    }
+                    default:
+                    {
+                        return munroData.Where(element => element.Category.Length > 0).ToList();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                return null;
+            }
         }
 
     }
--- MunroLibraryTest/QueriesTest.cs
+++ MunroLibraryTest/QueriesTest.cs
@@ -1,8 +1,10 @@
 ﻿using MunroLibrary;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Text;
+using MunroLibrary.Model;
 using Xunit;
 
 namespace MunroLibraryTest
@@ -10,12 +12,55 @@ namespace MunroLibraryTest
     public class QueriesTest
     {
         public CSVParcer TestParcer { get; set; }
+        List<string> TestHeaderList { get; set; }
+        List<string[]> TestDataList { get; set; }
+
+        private List<Munro> TestMunroData { get;set;}
+        private static List<Munro> emptyList { get; set; }
 
         private void  InitParcer()
         {
             string fileName = "test.csv";
             string path = Path.Combine(Environment.CurrentDirectory, @"Resources\", fileName);
             TestParcer = new CSVParcer();
+            TestHeaderList = new List<string>() { "Name", "Height (m)", "post 1997", "Grid Ref" };
+            TestDataList = new List<string[]>()
+            {
+                new string[] {"A", "10", "TOP", "NN"},
+                new string[] {"B", "20", "MUN", "NM"},
+                new string[] {"C", "30", "", "NO"}
+            };
+
+            TestMunroData = TestParcer.PopulateDataToObject(TestHeaderList, TestDataList);
+
+            emptyList = new List<Munro>();
+        }
+
+        [Fact]
+        public void DataIsNullOrEmpty()
+        {
+            //Arrange
+            InitParcer();
+
+            //Act 
+            var query = new MunroQueries();
+            var result = query.SortByCategory(null, MunroLibrary.Enums.HillCategory.Munro);
+            //Assert
+            Assert.Null(result);
+           
+        }
+
+        [Fact]
+        public void DataIsEmpty()
+        {
+            //Arrange
+            InitParcer();
+
+            //Act 
+            var query = new MunroQueries();
+            var result = query.SortByCategory(new List<Munro>(), MunroLibrary.Enums.HillCategory.Munro);
+            //Assert
+            Assert.Null(result);
         }
 
         [Fact]
@@ -23,26 +68,24 @@ namespace MunroLibraryTest
         {
             //Arrange
             InitParcer();
-
+           
             //Act 
             var query = new MunroQueries();
-            var result = query.SortByCategory(MunroLibrary.Enums.HillCategory.Munro);
+            var result = query.SortByCategory(TestMunroData, MunroLibrary.Enums.HillCategory.Munro);
 
-            var haveOnlyMunro = 0;
+            var haveOtherCategories = 0;
             foreach (var element in result)
             {
-                if (!element[element.Length - 2].Equals("MUN"))
+                if (!element.Category.Equals("MUN"))
                 {
-                    haveOnlyMunro++;
+                    haveOtherCategories++;
                 }
             }
 
-
             //Assert
             Assert.NotNull(result);
-            Assert.Equal(0, haveOnlyMunro);
-
-
+            Assert.Equal("MUN", result[0].Category);
+            Assert.Equal(0, haveOtherCategories);
         }
 
         [Fact]
@@ -53,21 +96,21 @@ namespace MunroLibraryTest
 
             //Act 
             var query = new MunroQueries();
-            var result = query.SortByCategory(MunroLibrary.Enums.HillCategory.MunroTop);
+            var result = query.SortByCategory(TestMunroData, MunroLibrary.Enums.HillCategory.MunroTop);
 
-            var haveOnlyMunroTop = 0;
+            var haveOtherCategories = 0;
             foreach (var element in result)
             {
-                if (!element[element.Length - 2].Equals("TOP"))
+               
+                if (!element.Category.Equals("TOP"))
                 {
-                    haveOnlyMunroTop++;
+                    haveOtherCategories++;
                 }
             }
 
-
             //Assert
             Assert.NotNull(result);
-            Assert.Equal(0, haveOnlyMunroTop);
+            Assert.Equal(0, haveOtherCategories);
         }
 
         [Fact]
@@ -78,17 +121,16 @@ namespace MunroLibraryTest
 
             //Act 
             var query = new MunroQueries();
-            var result = query.SortByCategory(MunroLibrary.Enums.HillCategory.MunroTop);
+            var result = query.SortByCategory(TestMunroData, MunroLibrary.Enums.HillCategory.Ether);
 
             var haveEmptyField = 0;
             foreach (var element in result)
             {
-                if (element[element.Length - 2].Equals(""))
+                if (element.Category.Length == 0)
                 {
                     haveEmptyField++;
                 }
             }
-
 
             //Assert
             Assert.NotNull(result);
